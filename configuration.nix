1428ed38322b9f4deaec872075106e148d791f5e
Jimbo <jimjam4real@gmail.com>
2024-09-24 00:06:34 -0400

    Small formatting

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Help is available in the configuration.nix(5) man page and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
@@ -9,8 +7,6 @@ let
   homeManager = fetchTarball
     "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
 in
-
-
 
 {
   imports =
