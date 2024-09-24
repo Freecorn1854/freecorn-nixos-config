e5b6256586d6f0dba5cd0c5a85c0061887c0c726
Freecorn <freecorn1854@jimbosfiles.com>
2024-09-23 21:23:11 -0600

    add files lmao

---
+++ plugins/users/freecorn.nix
@@ -0,0 +1,44 @@
+let
+  secrets = import ./secrets.nix;
+in {
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.freecorn = {
+    description = "FreeCorn";
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = secrets.cornKeys;
+    extraGroups = [ 
+      "networkmanager"
+      "wheel"
+      "video"
+      "scanner"
+      "lp"
+      "plugdev"
+      "nginx"
+      "nfsShare"
+      "docker"
+    ];
+  };
+
+  # Define home manager programs and configs
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.freecorn = { config, pkgs, ... }: {
+      # Install user programs
+      home.packages = (with pkgs; [
+        rustdesk-flutter anydesk vlc
+      ]);
+
+      # OBS with plugins
+      programs.obs-studio = {
+        enable = true;
+        plugins = with pkgs.obs-studio-plugins; [
+          advanced-scene-switcher obs-multi-rtmp
+        ];
+      };
+
+      # Don't change this
+      home.stateVersion = "24.05";
+    };
+  };
+}
