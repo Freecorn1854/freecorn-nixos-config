bb3271049403e17631cd2d814666b038f383b1b0
Jimbo <jimjam4real@gmail.com>
2024-09-30 00:37:05 -0400

    Idk bruh try this

---
+++ home/sway/swaybar.nix
@@ -0,0 +1,20 @@
+{config, pkgs, outputs, ...}: {
+  wayland.windowManager.sway = {
+    config.bars.top = {
+      position = "top";
+      statusCommand = null;
+    };
+  };
+  programs.i3status = {
+    enable = true;
+    modules = {
+      "ipv6".enable = false;
+      "disk /".enable = false;
+      "run_watch DHCP".enable = false;
+      "run_watch VPN".enable = false;
+      "wireless wlan0".enable = false;
+      "ethernet eth0".enable = false;
+      "battery 0".enable = false;
+    };
+  };
+}
