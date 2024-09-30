a8c1fa74af88704225d926a1f251486f6b7502a4
FreeCorn <freecorn1854@jimbosfiles.com>
2024-09-30 00:56:02 -0600

    edited the time on i3whatever its called

--- home/sway/swaybar.nix
+++ home/sway/swaybar.nix
@@ -1,2 +1,34 @@
 {config, pkgs, outputs, ...}: {
+  wayland.windowManager.sway = {
+    config.bars = [
+      {
+        position = "top";
+	colors.background = "#20202000";
+	statusCommand = "i3status";
+	fonts = {
+	  names = ["Ubuntu"];
+	  size = 11.5;
+	};
+      }
+    ];
+  };
+  programs.i3status = {
+    enable = true;
+    general = {
+      interval = 1;
+    };
+    modules = {
+      "ipv6".enable = false;
+      "wireless _first_".enable = false;
+      "ethernet _first_".enable = false;
+      "battery all".enable = false;
+      "disk /".enable = false;
+      "tztime local" = {
+       settings = {
+         format = "%I:%M";
+	 };
+      };
+    };
+  };
+}
 
