28a556144ee1c0240b0cd4a71cb4833cef7750c0
FreeCorn <freecorn1854@jimbosfiles.com>
2024-09-30 23:43:37 -0600

    NOW fixed issue with rules.nix

--- home/sway/rules.nix
+++ home/sway/rules.nix
@@ -5,38 +5,12 @@
       # Rules
       window = {
         border = outputs.look.border.int;
-        titlebar = false;
+        titlebar = true;
         commands = [
            {
             criteria = {title = "^GlobalShot";};
             command = ''floating enable, fullscreen enable global'';
           }
-          # Give apps that don't have them borders
-          {
-            criteria = {con_mark = "borderless";};
-            command = ''border pixel ${outputs.look.border.string}'';
-          }
-          {
-            criteria = {app_id = "firefox";};
-            command = ''mark borderless'';
-          }
-          {
-            criteria = {class = "steam";};
-            command = ''mark borderless'';
-          }
-          {
-            criteria = {app_id = "swappy";};
-            command = ''mark borderless'';
-          }
-          {
-            criteria = {app_id = "virt-manager";};
-            command = ''mark borderless'';
-          }
-          {
-            criteria = {window_role = "pop-up";};
-            command = ''mark borderless'';
-          }
-
         ];
       };
    };
