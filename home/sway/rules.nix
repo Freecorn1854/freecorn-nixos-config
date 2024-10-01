81f6fb8189950e1e682d1e44f7ee6d176da238bc
FreeCorn <freecorn1854@jimbosfiles.com>
2024-09-30 23:36:10 -0600

    updated rules.nix

--- home/sway/rules.nix
+++ home/sway/rules.nix
@@ -11,6 +11,32 @@
             criteria = {title = "^GlobalShot";};
             command = ''floating enable, fullscreen enable global'';
           }
+          # Give apps that don't have them borders
+          {
+            criteria = {con_mark = "borderless";};
+            command = ''border pixel ${outputs.look.border.string}'';
+          }
+          {
+            criteria = {app_id = "firefox";};
+            command = ''mark borderless'';
+          }
+          {
+            criteria = {class = "steam";};
+            command = ''mark borderless'';
+          }
+          {
+            criteria = {app_id = "swappy";};
+            command = ''mark borderless'';
+          }
+          {
+            criteria = {app_id = "virt-manager";};
+            command = ''mark borderless'';
+          }
+          {
+            criteria = {window_role = "pop-up";};
+            command = ''mark borderless'';
+          }
+
         ];
       };
    };
