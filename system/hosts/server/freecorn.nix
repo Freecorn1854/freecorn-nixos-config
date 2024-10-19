375856760c0154a6fc960672c1e8c6e33450fb31
Freecorn <freecorn1854@jimbosfiles.com>
2024-10-19 13:34:30 -0600

    ok pls work

--- system/hosts/server/freecorn.nix
+++ system/hosts/server/freecorn.nix
@@ -1,5 +1,5 @@
 let
-  secrets = import ./secrets.nix;
+#  secrets = import ./secrets.nix;
 in {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.freecorn = {
--- system/hosts/server/jimbo.nix
+++ system/hosts/server/jimbo.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  secrets = import ./secrets.nix;
+#  secrets = import ./secrets.nix;
 in {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jimbo = {
