09696453341f6f9a6612d3a2185446b029fb6a81
Freecorn <freecorn1854@jimbosfiles.com>
2024-10-19 13:37:51 -0600

    ok 2 pls work

--- system/hosts/server/freecorn.nix
+++ system/hosts/server/freecorn.nix
@@ -25,26 +25,6 @@ in {
   };
 
   # Define home manager programs and configs
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.freecorn = { config, pkgs, ... }: {
-  #    imports = [ 
-    #   ./zsh.nix
-     #  ./neovim.nix 
-#      ];
-      # Install user programs
-      home.packages = (with pkgs; [
-        rustdesk-flutter anydesk vlc
-      ]);
-
-      # OBS with plugins
-      programs.obs-studio = {
-        enable = true;
-        plugins = with pkgs.obs-studio-plugins; [
-          advanced-scene-switcher obs-multi-rtmp
-        ];
-      };
 
       # Don't change this
       home.stateVersion = "24.05";
